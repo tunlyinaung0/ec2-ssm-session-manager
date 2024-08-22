fa8d7620a9f15065b19b1d652adaf1f093dc7540
tunlyinaung0 <tunlyinaung@gmail.com>
2024-08-22 09:32:30 +0630

    changed azs and ami value

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "private_subnet_a" {
 resource "aws_subnet" "private_subnet_b" {
     vpc_id = var.vpc_id
     cidr_block = var.subnet_cidr_b
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-1b"
 
     tags = {
         Name = "${var.prefix}-private-subnet-b"
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,5 +3,5 @@ vpc_cidr = "10.0.0.0/16"
 subnet_cidr_a = "10.0.0.0/24"
 subnet_cidr_b = "10.0.1.0/24"
 
-ami = "ami-0a0e5d9c7acc336f1"
+ami = "ami-066784287e358dad1"
 instance_type = "t2.micro"
